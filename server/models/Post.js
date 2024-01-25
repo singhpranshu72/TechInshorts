563270ea09ec9e0b26ba245461209de818e863f8
Pranshu Singh <110157886+singhpranshu72@users.noreply.github.com>
2024-01-25 14:25:17 +0530

    Add files via upload

---
+++ server/models/Post.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const PostSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+   updatedAt: {
+    type: Date,
+    default: Date.now
+  },
+  // createdBy: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: "users",
+  // }
+ 
+});
+
+module.exports = mongoose.model('Post', PostSchema);
